217c51b137eeef101eefabe7571fabc2d7269263
Kang Gyu <kanggyu94@naver.com>
2015-02-07 00:41:27 +0900

    Clean project & Upgrade library

--- src/com/wafflestudio/babbabdirara/CafeInfoDialog.java
+++ src/com/wafflestudio/babbabdirara/CafeInfoDialog.java
@@ -1,19 +1,17 @@
-package com.wafflestudio.babbabdrara;
-
-import java.util.HashMap;
+package com.wafflestudio.babbabdirara;
 
 import android.app.Dialog;
 import android.content.Context;
 import android.view.Window;
 import android.widget.TextView;
 
-public class CafeInfoDialog extends Dialog
-{
+import java.util.HashMap;
+
+public class CafeInfoDialog extends Dialog {
 	private TextView hourDetails;
 	private TextView locationDetails;
 	
-	public CafeInfoDialog(Context context, String cafeName, HashMap<String, String> operatingHours, HashMap<String, String> locations)
-	{
+	public CafeInfoDialog(Context context, String cafeName, HashMap<String, String> operatingHours, HashMap<String, String> locations) {
 		super(context);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.cafe_info);
