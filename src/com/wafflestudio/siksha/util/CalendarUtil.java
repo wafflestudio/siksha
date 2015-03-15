1725cbf5aa37f3b91d0f81bb579e9fca76897d18
Gyeongin <gyeonginyu@gmail.com>
2015-03-15 22:10:57 +0900

    Widget download failure handling

--- src/com/wafflestudio/siksha/util/CalendarUtil.java
+++ src/com/wafflestudio/siksha/util/CalendarUtil.java
@@ -15,16 +15,6 @@ public class CalendarUtil {
     return calendar.get(Calendar.YEAR) + "년 " + (calendar.get(Calendar.MONTH) + 1) + "월 " + calendar.get(Calendar.DAY_OF_MONTH) + "일";
   }
 
-  public static String getCurrentTime() {
-    Calendar calendar = Calendar.getInstance();
-    return calendar.get(Calendar.HOUR_OF_DAY) + "시 " + calendar.get(Calendar.MINUTE) + "분 " + calendar.get(Calendar.SECOND) + "초";
-  }
-
-  public static int getCurrentSecond() {
-    Calendar calendar = Calendar.getInstance();
-    return calendar.get(Calendar.SECOND);
-  }
-
   public static int getCurrentHour() {
     Calendar calendar = Calendar.getInstance();
     return calendar.get(Calendar.HOUR_OF_DAY);
