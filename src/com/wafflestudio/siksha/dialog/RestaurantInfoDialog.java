aaa0abfb02110138c0298cc1ad3ed56b44906cb4
kanggyu94 <kanggyu94@naver.com>
2015-03-18 14:14:17 +0900

    Design restaurant infomation dialog

--- src/com/wafflestudio/siksha/dialog/RestaurantInfoDialog.java
+++ src/com/wafflestudio/siksha/dialog/RestaurantInfoDialog.java
@@ -2,36 +2,37 @@ package com.wafflestudio.siksha.dialog;
 
 import android.app.Dialog;
 import android.content.Context;
-import android.view.Window;
 import android.widget.TextView;
 
 import com.wafflestudio.siksha.R;
 import com.wafflestudio.siksha.util.FontUtil;
 import com.wafflestudio.siksha.util.RestaurantInfo;
 
-import java.util.HashMap;
-
 public class RestaurantInfoDialog extends Dialog {
+  private TextView restaurantView;
+
   private TextView operatingHoursTitle;
   private TextView locationTitle;
 	private TextView operatingHoursDetails;
 	private TextView locationDetails;
 
 	public RestaurantInfoDialog(Context context, String restaurantName) {
-		super(context);
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		setContentView(R.layout.restaurant_info_dialog);
+		super(context, R.style.restaurant_info_dialog);
+    setContentView(R.layout.restaurant_info_dialog);
 
+    restaurantView = (TextView) findViewById(R.id.restaurant_info_dialog_title);
     operatingHoursTitle = (TextView) findViewById(R.id.operating_hour);
     locationTitle = (TextView) findViewById(R.id.location);
     operatingHoursDetails = (TextView) findViewById(R.id.operating_hour_details);
 		locationDetails = (TextView) findViewById(R.id.location_details);
 
+    restaurantView.setTypeface(FontUtil.fontAPAritaDotumSemiBold);
     operatingHoursTitle.setTypeface(FontUtil.fontAPAritaDotumSemiBold);
     locationTitle.setTypeface(FontUtil.fontAPAritaDotumSemiBold);
     operatingHoursDetails.setTypeface(FontUtil.fontAPAritaDotumMedium);
     locationDetails.setTypeface(FontUtil.fontAPAritaDotumMedium);
 
+    restaurantView.setText(restaurantName);
     operatingHoursDetails.setText(RestaurantInfo.operatingHoursMap.get(restaurantName));
 		locationDetails.setText(RestaurantInfo.locationsMap.get(restaurantName));
 	}
