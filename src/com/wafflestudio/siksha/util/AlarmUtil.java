153443ef17d96039a5754951fb9f7a7f9696dd80
Kang Gyu <kanggyu94@gmail.com>
2015-03-27 04:45:14 +0900

    Apply setRepeating in alarm manager

--- src/com/wafflestudio/siksha/util/AlarmUtil.java
+++ src/com/wafflestudio/siksha/util/AlarmUtil.java
@@ -29,7 +29,7 @@ public class AlarmUtil {
     calendar.set(Calendar.HOUR_OF_DAY, 0);
     calendar.set(Calendar.MINUTE, 10);
 
-    alarmManager.setInexactRepeating(AlarmManager.RTC, calendar.getTimeInMillis(), cycleTime, sender);
+    alarmManager.setRepeating(AlarmManager.RTC, calendar.getTimeInMillis(), cycleTime, sender);
     SharedPreferenceUtil.save(context, SharedPreferenceUtil.PREF_ALARM_NAME, SharedPreferenceUtil.PREF_KEY_JSON, true);
   }
 }
