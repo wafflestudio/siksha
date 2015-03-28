c37c9fa0067943a1ca589c656101e4bf786d48a3
Gyeongin <gyeonginyu@gmail.com>
2015-03-29 01:08:45 +0900

    code rearrange

--- src/com/wafflestudio/siksha/widget/WidgetRemoteService.java
+++ src/com/wafflestudio/siksha/widget/WidgetRemoteService.java
@@ -1,14 +1,11 @@
 package com.wafflestudio.siksha.widget;
 
-import android.appwidget.AppWidgetManager;
 import android.content.Intent;
 import android.widget.RemoteViewsService;
 
 public class WidgetRemoteService extends RemoteViewsService {
     @Override
     public RemoteViewsFactory onGetViewFactory(Intent intent) {
-        int appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
-
         return (new WidgetListViewFactory(this.getApplicationContext(), intent));
     }
 }
