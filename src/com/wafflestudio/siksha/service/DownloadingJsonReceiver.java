4335f15c07f76efb2d56aeec9e89e5d1d8a34584
Gyeongin <gyeonginyu@gmail.com>
2015-03-30 23:49:09 +0900

    Download retry dialog bug

--- src/com/wafflestudio/siksha/service/DownloadingJsonReceiver.java
+++ src/com/wafflestudio/siksha/service/DownloadingJsonReceiver.java
@@ -3,6 +3,7 @@ package com.wafflestudio.siksha.service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class DownloadingJsonReceiver extends BroadcastReceiver {
   public static final String ACTION_PRE_DOWNLOAD = "com.wafflestudio.siksha.DownloadingJson.PRE_DOWNLOAD";
@@ -23,6 +24,7 @@ public class DownloadingJsonReceiver extends BroadcastReceiver {
   public void onReceive(Context context, Intent intent) {
     boolean isSuccess = intent.getBooleanExtra("is_success", false);
     String action = intent.getAction();
+    Log.d("DownloadingReciever_Action : ", action);
 
     if (action != null && action.equals(ACTION_CURRENT_DOWNLOAD)) {
       if (isSuccess)
